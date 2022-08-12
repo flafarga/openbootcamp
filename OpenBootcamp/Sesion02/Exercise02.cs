94293f8290d17d7d3d9fc170ce736cb0235ef9a3
Fran <francisco.lafarga@hotmail.com>
2022-08-12 17:34:06 +0200

    Add exercise 2 from session 2

---
+++ OpenBootcamp/Sesion02/Exercise02.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion02
+{
+    public class Exercise02
+    {
+        struct Coche
+        {
+            public int puertas;
+            public int ruedas;
+            public string marca;
+            public bool itv;
+        }
+
+        struct Mesa
+        {
+            public double peso;
+            public double largo;
+            public string material;
+            public string color;
+        }
+
+        public void printData()
+        {
+            var coche = new Coche() { puertas = 4, ruedas = 4, marca = "Nissan", itv = true };
+            Mesa mesa = new Mesa() { peso = 20.00, largo = 100, material = "Madera", color = "Marron" };
+            Console.WriteLine("Coche con {0} puertas, {1} ruedas, de marca {2} y con la ITV: {3}",
+                                coche.puertas, coche.ruedas, coche.marca, coche.itv);
+        }
+    }
+}
--- OpenBootcamp/Sesion02/Program.cs
+++ OpenBootcamp/Sesion02/Program.cs
@@ -6,11 +6,18 @@ namespace Sesion02
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("---Exercise 1---");
             Exercise01 ex1 = new Exercise01();
 
             ex1.getData();
             ex1.showData();
 
+            Console.WriteLine("---Exercise 2---");
+            Exercise02 ex2 = new Exercise02();
+
+            ex2.printData();
+
+            Console.WriteLine("---Exercise 3---");
         }
     }
 }
